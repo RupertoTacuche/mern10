a136781b0f2934b4a35058e6eb69f475b62702b2
@RupertoTacuche <murcio2490@gmail.com>
2024-03-12 14:27:08 -0600

    base_Datos_conectada

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,22 @@
-export const register = (req, res) => {
-    console.log(req.body)
-    res.send('registrando')
+import User from '../models/user.model.js'
+
+
+export const register = async (req, res) => {
+    const {email, password, username}= req.body
+
+try {
+
+    const newUser = new User({
+        username, 
+        email,
+        password
+    })
+
+   await newUser.save()
+   res.send('registrando')
+} catch (error) {
+    console.log(error)
+}
+    
 };
 export const login = (req, res) => res.send("login")
