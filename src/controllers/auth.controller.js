678ad3820161ad57573378edff42b9c24d62857e
@RupertoTacuche <murcio2490@gmail.com>
2024-03-14 11:23:50 -0600

    profile_funciona

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -74,4 +74,19 @@ export const logout = (req, res) => {
         expires: new Date(0)
     })
     return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+   const userFound =  await User.findById(req.user.id)
+
+   if(!userFound) return res.status(400).json({message: "User not found"});
+    //res.send('profile')
+
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    })
 }
